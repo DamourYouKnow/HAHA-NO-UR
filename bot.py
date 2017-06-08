b41369f116b34e0d8d717b35b9560f30af30adbe
Umi <982341777@qq.com>
2017-06-08 02:50:55 -0400

    Added proper logging, added a config file.
     @DamourYouKnow fill in config/sample_config.json and rename it to config.json

--- bot.py
+++ bot.py
@@ -1,8 +1,12 @@
+from pathlib import Path
+from time import time
+from traceback import format_exc
+
 from discord import Game
-from discord.ext.commands import Bot
+from discord.ext.commands import Bot, CommandNotFound, CommandOnCooldown
 from websockets.exceptions import ConnectionClosed
 
-from logger import log_request
+from logger import command_formatter, setup_logging, timestamp
 
 
 class HahaNoUR(Bot):
@@ -12,6 +16,10 @@ class HahaNoUR(Bot):
 
         :param prefix: the bot prefix
         """
+        self.prefix = prefix
+        self.log_path = Path(__file__).joinpath('logs')
+        self.start_time = time()
+        self.logger = setup_logging(self.start_time, self.log_path)
         super().__init__(prefix)
 
     async def on_ready(self):
@@ -40,4 +48,46 @@ class HahaNoUR(Bot):
         if message.author.bot:
             return
         await super().process_commands(message)
-        log_request(message, 'request')
+        content = message.content
+        command_name = content.split(' ')[0][len(self.prefix):]
+        if 'scout' in command_name.lower():
+            self.logger.log(
+                'INFO',
+                timestamp() + ' | ' + command_formatter(message, command_name)
+            )
+
+    async def on_command_error(self, exception, context):
+        """
+        Custom command error handling
+        :param exception: the expection raised
+        :param context: the context of the command
+        """
+        if isinstance(exception, CommandOnCooldown):
+            await self.handle_command_error(exception, context)
+        elif isinstance(exception, CommandNotFound):
+            # Ignore this case
+            return
+        else:
+            try:
+                # Raise it again so we don't lose traceback
+                raise exception
+            except:
+                tb = format_exc()
+                triggered = context.message.content
+                ex_type = type(exception).__name__
+                four_space = ' ' * 4
+                str_ex = str(exception)
+                msg = '\n{0}Triggered message: {1}\n' \
+                      '{0}Type: {2}\n' \
+                      '{0}Exception: {3}\n{4}' \
+                    .format(four_space, triggered, ex_type, str_ex, tb)
+                self.logger.log('WARNING', msg)
+
+    async def handle_command_error(self, exception, context):
+        """
+        Handle expected errors so no traceback is printed
+        :param exception: the exception raised.
+        :param context: the context of the exception.
+        """
+        if isinstance(exception, CommandOnCooldown):
+            await self.send_message(context.message.channel, str(exception))
--- main.py
+++ main.py
@@ -1,12 +1,19 @@
+from json import loads
+from pathlib import Path
+
 from bot import HahaNoUR
 from bot_commands import BotCommands
 
-if __name__ == '__main__':
-    with open("token.txt", "r") as f:
-        token = f.read().strip('\n')
-        f.close()
 
-    bot = HahaNoUR('!')
+def main():
+    with Path('config/config.json').open() as f:
+        config = loads(f)
+        f.close()
+    bot = HahaNoUR(config['default_prefix'])
     bot.remove_command('help')
     bot.add_cog(BotCommands(bot))
-    bot.run(token)
+    bot.run(config['token'])
+
+
+if __name__ == '__main__':
+    main()
