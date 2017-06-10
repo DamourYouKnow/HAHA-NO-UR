a860d9ca6558ff7313c69fbb816931911cd13f86
Umi <982341777@qq.com>
2017-06-10 03:07:24 -0400

    formatting

--- scout.py
+++ scout.py
@@ -100,7 +100,7 @@ def _parse_arguments(args: tuple) -> list:
     for arg in args:
         parsed_arg = _parse_argument(arg)
 
-        if arg != None:
+        if arg:
             parsed_args.append(parsed_arg)
 
     return parsed_args
@@ -319,7 +319,7 @@ class Scout:
             if arg_type == "main_unit":
                 request_url += '&idol_main_unit=' + arg_value
             elif arg_type == "sub_unit":
-                request_url += '&idol_sub_unit='+ arg_value
+                request_url += '&idol_sub_unit=' + arg_value
             elif arg_type == "name":
                 request_url += "&name=" + arg_value
             elif arg_type == "year":
