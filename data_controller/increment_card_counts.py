d057a2854555fef565292d4f40ab1ebb33ad16e8
D'Amour <damourba@gmail.com>
2017-07-25 18:30:43 -0400

    update to increment script

--- data_controller/increment_card_counts.py
+++ data_controller/increment_card_counts.py
@@ -5,7 +5,10 @@ db = DatabaseController()
 ids = db.get_all_user_ids()
 
 for user_id in ids:
+    print(user_id)
     user = User()
     user.id = user_id
     album = db.get_user_album(user)
     db.add_to_user_album(user, album)
+
+print("done")
