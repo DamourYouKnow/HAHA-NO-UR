6ca660e8b1e16001dc34f15b4425ff3b7184dbe6
D'Amour <damourba@gmail.com>
2017-07-29 16:59:42 -0400

    re-added the thing that gets the list of all command strings at launch. Don't delet this.

--- bot/bot.py
+++ bot/bot.py
@@ -50,6 +50,46 @@ class HahaNoUR(Bot):
             self.add_cog(cog)
         self.run(token)
 
+    def get_command_collections(self) -> tuple:
+        """
+        Return a list and a dict of the bot commands.
+        :param bot: the bot.
+        :return: A tuple of (list of bot commands, dict of bot commands)
+        The list contains tuples of (tuple of command name, command help)
+        The dict has cog name as key and list of tuple of name as val
+        """
+        aliases = []
+        command_list = []
+        command_dict = {}
+        for key, val in self.commands.items():
+            cog_name = val.cog_name
+            command_help = val.help
+            name = None
+            if key.startswith('_'):
+                aliases += val.aliases
+                name = tuple([n for n in val.aliases if not n.startswith('_')])
+            elif str(val) not in aliases and not str(val).startswith('_'):
+                name = (str(val),)
+            if name:
+                command_list.append((name, command_help))
+                if cog_name not in command_dict:
+                    command_dict[cog_name] = []
+                command_dict[cog_name].append(name)
+        return command_list, command_dict
+
+    def get_valid_commands(self):
+        """
+        Return a list of every valid user command, including aliases.
+
+        :return: Command list.
+        """
+        cmds = []
+        for cmd_and_aliases in self.get_command_collections()[1].values():
+            for alias_tuple in cmd_and_aliases:
+                for alias in alias_tuple:
+                    cmds.append(alias)
+        return cmds
+
     async def __change_presence(self):
         """
         Change the "Playinng" status of the bot.
@@ -79,6 +119,7 @@ class HahaNoUR(Bot):
         self.logger.log(logging.INFO, 'Logged in')
         self.logger.log(logging.INFO, f'{len(self.servers)} servers detected')
         self.help_general, self.all_help = get_help(self)
+        self.all_commands = self.get_valid_commands()
         await self.__change_presence()
 
     async def process_commands(self, message):
@@ -91,7 +132,10 @@ class HahaNoUR(Bot):
 
         content = message.content
         command_name = content.split(' ')[0][len(self.prefix):]
+        print(command_name)
+        print(self.all_commands)
         if command_name in self.all_commands:
+            print("hello")
             log_entry = command_formatter(message, self.prefix + command_name)
             self.logger.log(logging.INFO, log_entry)
 
