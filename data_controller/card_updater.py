9f1a9c7ce76f9bb4d087d7b19476e3c708d3a4c6
D'Amour <damourba@gmail.com>
2017-09-09 16:17:15 -0400

    added task that fetches new cards every minute

--- data_controller/card_updater.py
+++ data_controller/card_updater.py
@@ -1,8 +1,25 @@
 import discord
 import asyncio
 
+MAX_UPDATE_SIZE = 15
+
 async def update_task(bot):
     await bot.wait_until_ready()
     while not bot.is_closed:
-        await bot.db.cards.get_card_ids()
-        await asyncio.sleep(30)
+        # Get card ids from database and api.
+        db_card_ids = set(await bot.db.cards.get_card_ids())
+        api_card_ids = set(await bot.session_manager.get_json(
+                'http://schoolido.lu/api/cardids'))
+        new_card_ids = list(api_card_ids - db_card_ids)
+
+        if len(new_card_ids) > 0:
+            new_card_ids = new_card_ids[:MAX_UPDATE_SIZE] # Request up to MAX_UPDATE_SIZE.
+
+            res = await bot.session_manager.get_json(
+                    'http://schoolido.lu/api/cards',
+                    {'ids': ','.join(str(i) for i in new_card_ids)})
+
+            for card in res['results']:
+                await bot.db.cards.upsert_card(card)
+
+        await asyncio.sleep(60)
