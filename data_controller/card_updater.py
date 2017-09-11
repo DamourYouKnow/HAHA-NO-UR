f367df380ef4567e1d2b366274dcd71ab1216fac
D'Amour <damourba@gmail.com>
2017-09-11 18:59:49 -0400

    get cards faster (temp)'

--- data_controller/card_updater.py
+++ data_controller/card_updater.py
@@ -23,7 +23,7 @@ async def update_task(bot):
                 if validate_card(card):
                     await bot.db.cards.upsert_card(card)
 
-        await asyncio.sleep(60)
+        await asyncio.sleep(10)
 
 
 def validate_card(card: dict) -> bool:
