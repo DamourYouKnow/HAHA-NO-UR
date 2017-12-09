41d3e2bbde61f94efdad77cccd34e2a714745084
D'Amour <damourba@gmail.com>
2017-12-08 21:58:41 -0500

    implemented basic template for displaying stat key value pairs

--- commands/stats_commands.py
+++ commands/stats_commands.py
@@ -1,3 +1,4 @@
+import discord
 from discord.ext import commands
 from core.checks import check_mongo
 from bot import HahaNoUR
@@ -14,7 +15,20 @@ class Stats:
         Description: |
             Provides stats about you.
         """
-        raise NotImplementedError
+        stats = []
+        album = await self.bot.db.users.get_user_album(ctx.message.author.id)
+        counter = AlbumCounter(album)
+        counter.run_count()
+        stats.append(('Unique cards collected', counter.distinct_count))
+        stats.append(('Total cards', counter.total_count))
+        stats.append(('Unidolized cards', counter.unidolized_count))
+        stats.append(('Idolized cards', counter.idolized_count))
+        for rarity, count in counter.rarity_counts.items():
+            stats.append((rarity + ' cards', count))
+
+        
+        emb = _create_embed('Your stats', stats)
+        await self.bot.send_message(ctx.message.channel, embed=emb)
 
     @commands.command(pass_context=True)
     @commands.cooldown(rate=3, per=30, type=commands.BucketType.user)
@@ -25,3 +39,30 @@ class Stats:
             Provides stats about the bot.
         """
         raise NotImplementedError
+
+
+class AlbumCounter:
+    def __init__(self, album):
+        self.album = album
+        self.rarity_counts = {'N': 0, 'R': 0, 'SR': 0, 'SSR': 0, 'UR': 0}
+        self.total_count = 0
+        self.unidolized_count = 0
+        self.idolized_count = 0
+        self.distinct_count = 0
+
+    def run_count(self):
+        # TODO: Refactor out _merge_card_info from album commands.
+        self.total_count = self.idolized_count + self.unidolized_count
+        self.distinct_count = len(self.album)
+
+
+def _create_embed(title: str, stats: list):
+    """
+    Create a stats embed.
+
+    :param title: Title of embed.
+    :param stats: List of tuples (stat name, stat value).
+    """
+    desc = '\n'.join([str(k) + ': ' + str(v) for k,v in stats])
+    emb = discord.Embed(title=title, description=desc)
+    return emb
