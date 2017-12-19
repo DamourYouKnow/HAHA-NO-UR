18f2ca222e0c7f428815d6b59d13efe93e46b0e2
D'Amour <damourba@gmail.com>
2017-12-18 19:41:49 -0500

    test 2

--- bot/bot.py
+++ bot/bot.py
@@ -56,7 +56,7 @@ class HahaNoUR(Bot):
         """
         try:
             await self.wait_until_ready()
-            await self.change_presence(game=Game(name='!help !info'))
+            await self.change_presence(game=Game(name='!help'))
         except ConnectionClosed:
             await self.logout()
             await self.login()
