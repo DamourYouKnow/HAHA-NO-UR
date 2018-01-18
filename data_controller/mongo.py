4678d0b935959002d5de5584c6286fe3b510db5e
damouryouknow <damourba@gmail.com>
2018-01-17 21:34:06 -0500

    added database controllers for server specific settings

--- data_controller/mongo.py
+++ data_controller/mongo.py
@@ -2,6 +2,7 @@ import motor.motor_asyncio
 from data_controller.user_controller import UserController
 from data_controller.card_controller import CardController
 from data_controller.feedback_controller import FeedbackController
+from data_controller.server_controller import ServerController
 
 PORT = 27017
 DATABASE_NAME = "haha-no-ur"
@@ -16,6 +17,7 @@ class MongoClient:
         self.users = UserController(self)
         self.cards = CardController(self)
         self.feedback = FeedbackController(self)
+        self.servers = ServerController(self)
 
     def __del__(self):
         """
---
+++ data_controller/server_controller.py
@@ -0,0 +1,22 @@
+from data_controller.database_controller import DatabaseController
+
+class ServerController(DatabaseController):
+    def __init__(self, mongo_client):
+        """
+        Constructor for a ServerController.
+
+        :param mongo_client: Mongo client used by this controller.
+        """
+        super().__init__(mongo_client, 'server')
+
+    async def set_prefix(self, server_id: str, prefix: str):
+        doc = {'_id': server_id}
+        set_prefix = {'$set': prefix}
+        await self._collection.update(doc, set_prefix, upsert=True)
+
+    async def get_prefix(self, server_id: str) -> str:
+        server = await self._collection.find_one({'_id': server_id})
+        if server and server['command_prefix']:
+            return server['command_prefix']
+        return '!'
+            
