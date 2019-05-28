4eced2935af20ecbfae203cc8a9b18d62c2571aa
damouryouknow <damourba@gmail.com>
2019-05-28 17:46:50 -0400

    Made command line shard arguments optional

--- commands/__init__.py
+++ commands/__init__.py
@@ -2,7 +2,6 @@ from commands.info_commands import Info
 from commands.album_commands import Album
 from commands.scout_commands import Scout
 from commands.stats_commands import Stats
-from commands.trivia_commands import Trivia
 from commands.config_commands import Config
 
-__all__ = ['Scout', 'Album', 'Info', 'Stats', 'Trivia', 'Config']
+__all__ = ['Scout', 'Album', 'Info', 'Stats', 'Config']
